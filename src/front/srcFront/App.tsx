baf2b2df17a3ad0dc24085a5c2f351a3ea9a45af
Juan123839 <diegoare20032@gmail.com>
2026-08-31 20:42:53 -0500

    💄style(frontend): ajustar estilo del login

--- src/front/srcFront/App.tsx
+++ src/front/srcFront/App.tsx
@@ -2,14 +2,23 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Perfil from "./Perfil";
 import Practica from "./Practica";
+import Login from "./Login";
+
 
 
 function App() {
 
-  const [pagina, setPagina] = useState("practica");
+  const [logueado, setLogueado] = useState(false);
+  const [pagina, setPagina] = useState("Practica");
 
-
+  if(!logueado){
+    return(
+      <Login cambiarPagina={() => setLogueado(true)}
+      />
+    );
+  }
   return (
+    
     <div className="app">
       <Navbar cambiarPagina={setPagina} paginaActual={pagina}/>
       <main>
