4151e8220978e855800dd607f51c6b851fd28489
David-wallpaper <dvallejo2007@hotmail.com>
2026-09-08 14:25:25 -0500

    :sparkles:feature(backend): se agrega un import al proceso de autenticacion para que se use el recuperar contraseña #145

--- app/routes/autenticacion.py
+++ app/routes/autenticacion.py
@@ -1,7 +1,7 @@
 import logging
 
 from fastapi import APIRouter, Depends, HTTPException, status
-
+from app.routes.recuperacion_contrasena import router as router_recuperacion
 from app.security import crear_token_acceso, obtener_usuario_actual
 from app.services.autenticacion_service import autenticar_usuario
 from src.schemas.autenticacion import (
