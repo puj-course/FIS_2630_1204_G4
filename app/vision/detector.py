5026ae45a98de1ac3c86e75e54696400841d4a62
David-wallpaper <dvallejo2007@hotmail.com>
2026-09-11 11:06:52 -0500

    :wrench:chore(vision): ajuste de archivos detector.py para separar responsabilidades #149

--- app/vision/detector.py
+++ app/vision/detector.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import cv2
 import mediapipe as mp
 
 
@@ -14,16 +15,21 @@ RUTA_MODELO = str(
 class DetectorMano:
 
     def __init__(self):
+
         opciones = mp.tasks.vision.HandLandmarkerOptions(
             base_options=mp.tasks.BaseOptions(
                 model_asset_path=RUTA_MODELO
             ),
+
             running_mode=mp.tasks.vision.RunningMode.VIDEO,
+
             num_hands=1,
+
             min_hand_detection_confidence=0.65,
             min_hand_presence_confidence=0.65,
             min_tracking_confidence=0.65,
         )
+
 
         self.detector = (
             mp.tasks.vision.HandLandmarker
@@ -32,7 +38,23 @@ class DetectorMano:
 
 
     def detectar(self, imagen, tiempo_actual):
-        return self.detector.detect_for_video(
+
+        # OpenCV trabaja con colores BGR
+        # MediaPipe necesita colores RGB
+        imagen_rgb = cv2.cvtColor(
             imagen,
-            tiempo_actual
+            cv2.COLOR_BGR2RGB
+        )
+
+
+        # Convertimos la imagen al formato esperado por MediaPipe
+        imagen_mediapipe = mp.Image(
+            image_format=mp.ImageFormat.SRGB,
+            data=imagen_rgb,
+        )
+
+
+        return self.detector.detect_for_video(
+            imagen_mediapipe,
+            tiempo_actual,
         )
